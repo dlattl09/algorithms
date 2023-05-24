8d7b2b00c86495d85308695e7ba0f391f36453c8
Heejoo Lim <54707924+dlattl09@users.noreply.github.com>
2023-05-24 16:53:50 +0900

    Update 성적평가.py

--- sort/성적평가.py
+++ sort/성적평가.py
@@ -32,3 +32,66 @@ print(*rank2)
 print(*rank3)
 print(*totalrank)
 
+
+# Merge sort를 사용한 경우
+# 여전히 시간제한 
+import sys
+ 
+n = int(sys.stdin.readline())
+
+def merge_sort(arr):
+    if len(arr) <2:
+        return arr
+    mid = len(arr) //2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+
+    merged_arr = []
+    l=h=0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] > high_arr[h]:
+            merged_arr.append(low_arr[l])
+            l+=1
+        else:
+            merged_arr.append(high_arr[h])
+            h+=1
+    merged_arr += low_arr[l:]
+    merged_arr += high_arr[h:]
+
+    return merged_arr
+
+
+results = []
+for _ in range(3):
+    results.append(list(map(int, sys.stdin.readline().split())))
+
+total = []
+rank1 = []
+rank2 = []
+rank3 = []
+totalrank = []
+
+for i in range(n):
+    total.append(results[0][i]+results[1][i]+results[2][i])
+
+for i in range(3):
+    rank1 = merge_sort(results[0])
+    rank2 = merge_sort(results[1])
+    rank3 = merge_sort(results[2])
+    totalrank = merge_sort(total)
+
+
+for i in results[0]:
+    print(rank1.index(i)+1, end = ' ')
+print()
+for i in results[1]:
+    print(rank2.index(i)+1, end = ' ')
+print()
+for i in results[2]:
+    print(rank3.index(i)+1, end = ' ')
+print()
+for i in total:
+    print(totalrank.index(i)+1, end = ' ')
+print()
+
+
